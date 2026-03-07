6d9bec1169ff1396cbe72802c53068453f4c3f8f
Mia-Cran <palmermaria1991@gmail.com>
2026-03-07 14:42:42 -0500

    Implement random quote display and button updates
    Added functionality to display random quotes and update button text.

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+ const quoteBtn = document.getElementById("quoteBtn");
+ const quoteContainer = document.getElementById("quoteContainer");
+
+ const quotes = [
+  "Believe you can and you're halfway there.",
+  "You are stronger than you think.",
+  "Small steps everyday lead to the big chage.",
+  "Dream it. Wish it. Do it.",
+  "Stay positive, work hard, make it happen."
+ ];
+
+ quoteBtn.addEventListener("click", function () {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+
+  const newQuote = document.createElement("p");
+  newQuote.textContent = randomQuote;
+
+  quoteContainer.textContent = randomQuote;
+ });
+ const button1 = document.getElementById("readBtn1");
+ const button2 = document.getElementById("readBtn2");
+ const button3 = document.getElementById("readBtn3");
+
+ readBtn1.addEventListener("click", function () {
+  readBtn1.textContent = "Read More";
+ })
+
+ readBtn2.addEventListener("click", function () {
+  readBtn2.textContent = "Read More";
+ })
+
+ readBtn3.addEventListener("click", function () {
+  readBtn3.textContent = "Read More";
+ })
+
+
 

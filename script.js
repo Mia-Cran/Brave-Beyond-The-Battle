179bc67531b8cbd8dba04d36078a1bd8ce913ef6
Mia-Cran <palmermaria1991@gmail.com>
2026-03-11 15:27:38 -0400

    Add event listeners for new buttons and comments

--- script.js
+++ script.js
@@ -21,18 +21,51 @@
  const button1 = document.getElementById("readBtn1");
  const button2 = document.getElementById("readBtn2");
  const button3 = document.getElementById("readBtn3");
+ const postMore = document.getElementById("PostMore");
+ const postAgain = document.getElementById("PostAgain");
+ const postLastTime = document.getElementById("PostLastTime");
+ const testimonialBtn = document.getElementById("testimonialBtn");
+ const testimonialText = document.getElementById("testimonialText");
+ const commentBtn = document.getElementById("commentBtn");
+ const commentInput = document.getElementById("commentInput");
+ const commentList = document.getElementById("commentList");
+ 
 
  readBtn1.addEventListener("click", function () {
   readBtn1.textContent = "Read More";
  })
+  postMore.addEventListener("click", function () {
+   postMore.style.display = "block";
+  })
 
  readBtn2.addEventListener("click", function () {
   readBtn2.textContent = "Read More";
  })
+  postAgain.addEventListener("click", function () {
+   postAgain.style.display = "block";
+  })
 
  readBtn3.addEventListener("click", function () {
   readBtn3.textContent = "Read More";
  })
+ postLastTime.addEventListener("click", function () {
+  postLastTime.style.display = "block";
+ })
+
+ testimonialBtn.addEventListener("click", function() {
+  testimonialText.style.display = "block";
+ });
+
+ commentBtn.addEventListener("click",function () {
+  const newComment = document.createElement("p");
+  newComment.textContent = commentInput.value;
+  commentList.appendChild(newComment);
+  commentInput.value = "";
+ })
+
+
+
+ 
 
 
 
